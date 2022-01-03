37d4db09a7f5ff1e51afb633738fb3c63f6a88f4
ben <ben@benrothman.org>
2022-01-02 19:46:22 -0500

    init

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['src/library/js/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+		sass: {
+        options: {
+            implementation: sass,
+            sourceMap: true
+        },
+        dist: {
+            files: {
+                'build/library/css/main.css': 'src/library/scss/main.scss'
+            }
+        }
+    },
+    watch: {
+      files: ['src/library/scss/main.scss'],
+      tasks: ['jshint', 'sass']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default', ['sass']);
+
+};
